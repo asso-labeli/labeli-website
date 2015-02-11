aeb74e98efd841f506eec64d58b37191f1bcc2a6
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-02-11 03:25:17 +0100

    Update server and hook scripts

--- hook.js
+++ hook.js
@@ -1,14 +1,11 @@
-var gith = require('gith').create(9002);
+var gith = require('gith').create();
 
 var execFile = require('child_process').execFile;
 
 gith({repo: 'eolhing/labeli-website'}).on('all', function(payload)
 {
     if(payload.branch === 'master')
-    {
-        execFile('./hook.sh', function(error, stdout, stderr)
-        {
-            console.log('update complete');
-        });
-    }
+        execFile('./hook.sh');
 });
+
+module.exports = gith;
--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var app = express();
 
-// static content
 app.use(express.static(__dirname));
+app.get('*', function(req, res){ res.sendFile(__dirname + '/index.html'); });
 
-// return index.html for all other routes
-app.get('*', function(req, res)
-{
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(80, function() {
-  console.log('server.js running');
-});
+module.exports = app;

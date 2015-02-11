b53c501aa61cc10e108a702677bcccd7c712a627
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-02-11 03:48:47 +0100

    Add hook as subdomain

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var vhost = require('vhost');
 var app = express();
+
+app.use(vhost('hook.website.labeli.org', require('./hook.js')));
 
 app.use(express.static(__dirname));
 app.get('*', function(req, res){ res.sendFile(__dirname + '/index.html'); });

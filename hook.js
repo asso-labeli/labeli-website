022ee813cfb6fbc892522ed549a524670971087f
Alix Fumoleau <alix.fumoleau@gmail.com>
2015-03-10 09:16:33 +0100

    Update hook

--- hook.js
+++ hook.js
@@ -1,11 +1,9 @@
-var gith = require('gith').create();
+var gith = require('gith').create(9002);
 
 var execFile = require('child_process').execFile;
 
-gith({repo: 'eolhing/labeli-website'}).on('all', function(payload)
+gith({repo: 'asso-labeli/labeli-website'}).on('all', function(payload)
 {
     if(payload.branch === 'master')
         execFile('./hook.sh');
 });
-
-module.exports = gith;
